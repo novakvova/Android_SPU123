946923613c7a91e37295f914cb1c9dbacba6a2e9
novakvova <novakvova@gmail>
2023-10-21 10:22:03 +0300

    Add Identity and Seed Data

--- WebStore/WebStore/Data/SeederDB.cs
+++ WebStore/WebStore/Data/SeederDB.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.Internal;
+using WebStore.Constants;
 using WebStore.Data.Entitties;
+using WebStore.Data.Entitties.Identity;
 
 namespace WebStore.Data
 {
@@ -12,6 +15,8 @@ namespace WebStore.Data
                 .GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
                 var context = scope.ServiceProvider.GetRequiredService<AppEFContext>();
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
                 context.Database.Migrate();
                 if(!context.Categories.Any()) 
                 {
@@ -34,6 +39,61 @@ namespace WebStore.Data
                     context.Categories.Add(laptop);
                     context.Categories.Add(clothes);
                     context.SaveChanges();
+                }
+
+                if (!context.Roles.Any())
+                {
+                    RoleEntity admin = new RoleEntity
+                    {
+                        Name = Roles.Admin
+                    };
+                    RoleEntity user = new RoleEntity
+                    {
+                        Name = Roles.User
+                    };
+                    var result = roleManager.CreateAsync(admin).Result;
+                    if (result.Succeeded)
+                    {
+                        Console.WriteLine("----Роль успішно створено---- {0}", admin.Name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("+++Помилка створення ролі+++ {0}", admin.Name);
+                    }
+                    result = roleManager.CreateAsync(user).Result;
+                    if (result.Succeeded)
+                    {
+                        Console.WriteLine("----Роль успішно створено---- {0}", user.Name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("+++Помилка створення ролі+++ {0}", user.Name);
+                    }
+
+                    
+                }
+            
+                if(!context.Users.Any())
+                {
+                    var ivan = new UserEntity
+                    {
+                        FirstName = "Марко",
+                        LastName = "Лисий",
+                        Email = "lysyi@gmail.com",
+                        UserName = "lysyi@gmail.com"
+                    };
+                    var result = userManager.CreateAsync(ivan, "123456").Result;
+                    if (result.Succeeded)
+                    {
+                        Console.WriteLine("-------Користувача успішно створено-------- {0}", ivan.Email);
+                        result = userManager.AddToRoleAsync(ivan, Roles.Admin).Result;
+                        if(result.Succeeded) { Console.WriteLine("---Користувач {0} отримав роль {1}----", ivan.Email, Roles.Admin); }
+                        else { Console.WriteLine("++++Помилка видчаі ролі {1} Користувачу {0}++++", ivan.Email, Roles.Admin); }
+                    }
+                    else
+                    {
+                        Console.WriteLine("+++++Помилка створення користувача++++++ {0}", ivan.Email);
+                    }
                 }
             }
         }
