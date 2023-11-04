4a01333c97ec2a35589f30c91259a1fdbee3c783
novakvova <novakvova@gmail>
2023-11-04 10:20:55 +0200

    Upload image

--- Lesson 1. Begin/app/src/main/java/com/example/mystore/contants/Urls.java
+++ Lesson 1. Begin/app/src/main/java/com/example/mystore/contants/Urls.java
@@ -1,5 +1,6 @@
 package com.example.mystore.contants;
 
 public class Urls {
-    public static final String Base="http://10.0.2.2:5190";
+//    public static final String Base="http://10.0.2.2:5190";
+    public static final String Base="https://spu123.itstep.click";
 }
